f71ae8e69212f49f5054a7a3749de7bb2d7b6e44
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-02 20:56:00 -0300

    crypto-installed-and-tested

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,19 +5,30 @@ import {
   FlatList,
   TextInput,
   TouchableOpacity,
+  Alert,
 } from "react-native"
+import { useState } from "react"
+import * as Crypto from "expo-crypto"
 import AntDesign from "@expo/vector-icons/AntDesign"
 
 import { styles } from "./styles"
 import { colors } from "../../constants/colors"
 
-import { TaskItem, TaskItemProps } from "../../components/TaskItem"
 import { EmptyTaskList } from "../../components/EmptyTaskList"
 import { TasksStatusBar } from "../../components/TasksStatusBar"
-import { useState } from "react"
+import { TaskItem, TaskItemProps } from "../../components/TaskItem"
 
 export function Home() {
   const [tasks, setTasks] = useState<TaskItemProps[]>([])
+  const [task, setTask] = useState<string>("")
+
+  function handleAddTask() {
+    if (!task)
+      return Alert.alert(
+        "Tarefa vazia",
+        "Você não pode adicionar uma tarefa vazia."
+      )
+  }
 
   return (
     <View style={styles.container}>
@@ -31,8 +42,13 @@ export function Home() {
             style={styles.textInput}
             placeholder="Adicione uma nova tarefa"
             placeholderTextColor={colors.gray[300]}
+            onChangeText={setTask}
+            value={task}
           />
-          <TouchableOpacity style={styles.addTaskButton}>
+          <TouchableOpacity
+            style={styles.addTaskButton}
+            onPress={handleAddTask}
+          >
             <AntDesign name="pluscircleo" size={16} color="white" />
           </TouchableOpacity>
         </View>
